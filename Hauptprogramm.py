3a4fd91d11494321d2abf4eed20d08f340101b0d
chrissch24 <christophscheele.halter@gmail.com>
2025-03-25 10:58:50 +0100

    Version1

---
+++ Hauptprogramm.py
@@ -0,0 +1,101 @@
+#Projekt: Smart Heizungssteuerung
+#Ersteller: Ch. Scheele
+#Erstellungsdatum: 25.03.2025
+#Letzte Änderung:
+
+#=====Bibliotheken=====#
+from machine import Pin, PWM, SoftI2C
+import time
+from aht10 import AHT10  # Temperatur- und Luftfeuchtigkeitssensor
+import CCS811 # Luftqualitätssensor
+#======================#
+
+#=====Pins definieren=====#
+# I2C-Pins definieren:
+i2c = SoftI2C(scl=Pin(1), sda=Pin(2))
+
+#=========================#
+
+#=====Sensor Objekte definieren=====#
+sensoraht10 = AHT10(i2c)
+sensorccs811 = CCS811.CCS811(i2c=i2c, addr=90)
+#===================================#
+
+#=====Variabeln festlegen=====#
+raumtemperatur = 0
+luftfeuchtigkeit = 0
+co2_wert = 0
+tvoc_wert = 0
+raumtemp = []
+raumluft = []
+co2_list = []
+tvoc_list = []
+#=============================#
+
+#=====Einstellungen=====#
+messloops = 10  # Anzahl der durchgeführten Messungen bei einem Messzyklus
+#=======================#
+
+#=====Funktionen=====#
+def messfilter(messwerte):
+    """Filtern von Messwerten, um Ausreißer zu entfernen"""
+    if len(messwerte) < 3:  # Sicherheit, um Fehler zu vermeiden
+        return sum(messwerte) / len(messwerte) if messwerte else 0
+    
+    messwerte.sort()  # Werte sortieren
+    messwerte.pop(0)  # Kleinster Wert entfernen
+    messwerte.pop()   # Größter Wert entfernen
+    messwerte = round(sum(messwerte) / len(messwerte), 2)
+    return messwerte
+
+#-------------------------------------------#
+# Messung der Temperatur und Luftfeuchtigkeit
+def messungaht10():
+    """Messung der Temperatur und Luftfeuchtigkeit"""
+    global raumtemperatur, luftfeuchtigkeit
+    try:
+        raumtemp.clear()
+        raumluft.clear()
+        for i in range(messloops):
+            raumtemp.append(sensoraht10.temperature())
+            raumluft.append(sensoraht10.humidity())
+        raumtemperatur = messfilter(raumtemp)
+        luftfeuchtigkeit = messfilter(raumluft)
+    except Exception as e:
+        print("Fehler beim Lesen des AHT10-Sensors:", e)
+        raumtemperatur = "Fehler"
+        luftfeuchtigkeit = "Fehler"
+
+#-------------------------------------------#
+# Messung der Luftqualität
+def messungccs811():
+    """Messung Luftqualität"""
+    global co2_wert, tvoc_wert
+    try:
+        for i in range(messloops):
+            co2_list.append(sensorccs811.eCO2)
+            tvoc_list.append(sensorccs811.tVOC)
+        #Messwerte filtern
+        co2_wert = messfilter(co2_list)
+        tvoc_wert = messfilter(tvoc_list)
+    
+    except Exception as e:
+        print("Fehler beim Lesen des CCS811-Sensors:", e)
+        co2_wert = "Fehler"
+        tvoc_wert = "Fehler"
+    
+    
+
+#====================#
+
+#=====Hauptschleife=====#
+while True:
+    # Temperatur und Luftfeuchtigkeit messen
+    messungaht10()
+
+    if sensorccs811.data_ready():
+        messungccs811()
+
+    # Ausgabe der gemessenen Werte
+    print(f"Temperatur: {raumtemperatur}°C, Luftfeuchtigkeit: {luftfeuchtigkeit}%, CO2-Wert: {co2_wert}ppm, TVOC-Wert: {tvoc_wert}ppb")
+
