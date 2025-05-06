0253fa31455695926281b59fadecb5e4ac2409ec
chrissch24 <christophscheele.halter@gmail.com>
2025-05-06 10:37:04 +0200

    Löschen alter Dateien

--- TestIR.py
+++
@@ -1,8 +0,0 @@
-from machine import Pin
-from ir_tx.nec import NEC     # Empfängerklasse
-
-ir_tx = NEC(Pin(4, Pin.OUT))
-
-print("Senden Starten")
-ir_tx.transmit(1,2)
-print("Sendung abgeschlossen")
